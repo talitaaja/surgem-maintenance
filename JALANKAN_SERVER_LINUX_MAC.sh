d46a25cb480566e1f368f162498fef2a6a3c0160
talita <talitaadzra09@gmail.com>
2026-07-28 16:08:51 +0700

    terbaru

--- JALANKAN_SERVER_LINUX_MAC.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-cd "$(dirname "$0")" || exit 1
-echo "Menjalankan SurGem Maintenance pada http://localhost:3000"
-exec node server.js
--- JALANKAN_SERVER_WINDOWS.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Menjalankan SurGem Maintenance pada http://localhost:3000
-node server.js
-pause
